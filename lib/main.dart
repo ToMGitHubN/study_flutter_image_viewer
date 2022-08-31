d14eaa0011c86cbeb73a360c220adaf0f0cb3d0c
tomita <tomita@1-10.com>
2022-08-31 16:26:01 +0900

    画像大UIだと分かりやすいように変更

--- lib/main.dart
+++ lib/main.dart
@@ -47,9 +47,9 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// 画像をメインサイズで大きく表示する
-class ViewImages extends StatefulWidget {
-  const ViewImages({Key? key, required this.title}) : super(key: key);
+// 画像を大きく(メインサイズ)表示する
+class MainView extends StatefulWidget {
+  const MainView({Key? key, required this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -63,10 +63,71 @@ class ViewImages extends StatefulWidget {
   final String title;
 
   @override
-  State<ViewImages> createState() => _ViewImages();
+  State<MainView> createState() => _MainView();
 }
 
-class _ViewImages extends State<ViewImages> {
+class _MainView extends State<MainView> {
+  String viewMode = "ListViewImages";//[ListViewImages or GridViewImages or BigImage]
+
+  ListViewImages listViewImages = ListViewImages(title: 'Flutter Demo Home Page');
+
+  void _changeView() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      viewMode = "BigImage";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: listViewImages,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _changeView,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+// 画像をメインサイズで大きく表示する
+class BigImage extends StatefulWidget {
+  const BigImage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<BigImage> createState() => _BigImage();
+}
+
+class _BigImage extends State<BigImage> {
   int _counter = 0;
 
   void _incrementCounter() {
